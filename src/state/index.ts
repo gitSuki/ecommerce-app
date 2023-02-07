21617614f1b461cbcec2abcf425943cf83bcb825
gitSuki <collinbrown226@gmail.com>
2023-02-06 20:46:27 -0600

    Create basic reducers for state

--- src/state/index.ts
+++ src/state/index.ts
@@ -1,6 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
+interface CartState {
+    isCartOpen: boolean;
+    cart: any[];
+    items: any[];
+}
+
+const initialState: CartState = {
     isCartOpen: false,
     cart: [],
     items: [],
@@ -13,8 +19,32 @@ export const cartSlice = createSlice({
         setItems: (state, action) => {
             state.items = action.payload;
         },
+        addToCart: (state, action) => {
+            state.cart = [...state.cart, action.payload.item];
+        },
+        removeFromCart: (state, action) => {
+            state.cart = state.cart.filter(item => item.id !== action.payload.id);
+        },
+        increaseItemCount: (state, action) => {
+            state.cart = state.cart.map(item => {
+                if (item.id === action.payload.id) {
+                    item.count++;
+                }
+            });
+        },
+        decreaseItemCount: (state, action) => {
+            state.cart = state.cart.map(item => {
+                if (item.id === action.payload.id && item.count > 1) {
+                    item.count--;
+                }
+            });
+        },
+        setCartIsOpen: state => {
+            state.isCartOpen = !state.isCartOpen;
+        },
     },
 });
 
-export const { setItems } = cartSlice.actions;
+export const { setItems, addToCart, removeFromCart, increaseItemCount, decreaseItemCount, setCartIsOpen } =
+    cartSlice.actions;
 export default cartSlice.reducer;

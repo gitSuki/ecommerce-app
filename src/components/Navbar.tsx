00326cac5a174b5297f8599bf3bc79ffe0f38bff
gitSuki <collinbrown226@gmail.com>
2023-02-06 20:26:14 -0600

    Install mui icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,18 @@
-const Navbar = (): JSX.Element => {
-  return (
-    <div>Navbar</div>
-  )
-}
+import { useDispatch, useSelector } from "react-redux";
+import { Box } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
-export default Navbar
+const Navbar = (): JSX.Element => {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    return (
+        <Box display="flex" alignItems="center" width="100%" height="60px" position="fixed" top="0" left="0" zIndex="1">
+            <Box display="flex" alignItems="center" justifyContent="center" margin="auto" width="80%">
+                <Box onClick={() => navigate("/")}>Title</Box>
+                <Box display="flex" justifyContent="space-between" columnGap="20px" zIndex="2"></Box>
+            </Box>
+        </Box>
+    );
+};
+
+export default Navbar;

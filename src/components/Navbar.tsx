a1e252565557ccebdb0f690cb21e6ddfa09d192a
gitSuki <collinbrown226@gmail.com>
2023-02-12 16:58:53 -0600

    Add search bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { setCartIsOpen } from "../state/cart";
-import { IconButton } from "@mui/material";
+import { IconButton, TextField } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import "./Navbar.css";
 
@@ -15,9 +15,16 @@ const Navbar = (): JSX.Element => {
         <nav className="navbar">
             <h1>Store Name</h1>
             <form className="search-section">
-                <input type="text" placeholder="Search items..." />
-                <IconButton sx={{ '&:hover': {backgroundColor: "RGBA(163,163,163,0.50)"}}} >
-                    <SearchIcon className="search-icon"></SearchIcon>
+                <TextField
+                    label="Search Items..."
+                    variant="filled"
+                    sx={{
+                        input: { color: "#cccccc", backgroundColor: "#262626" },
+                        label: { color: "#828181" },
+                    }}
+                />
+                <IconButton sx={{ "&:hover": { backgroundColor: "RGBA(163,163,163,0.50)" } }}>
+                    <SearchIcon sx={{ color: "#cccccc" }}></SearchIcon>
                 </IconButton>
             </form>
             <div>

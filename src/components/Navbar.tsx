f8b930c61bc708b43694ff1bd3b5bd1e716dfc40
gitSuki <collinbrown226@gmail.com>
2023-02-12 17:05:27 -0600

    Refactor search bar to vanilla

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { setCartIsOpen } from "../state/cart";
-import { IconButton, TextField } from "@mui/material";
+import { IconButton } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import "./Navbar.css";
 
@@ -15,14 +15,7 @@ const Navbar = (): JSX.Element => {
         <nav className="navbar">
             <h1>Store Name</h1>
             <form className="search-section">
-                <TextField
-                    label="Search Items..."
-                    variant="filled"
-                    sx={{
-                        input: { color: "#cccccc", backgroundColor: "#262626" },
-                        label: { color: "#828181" },
-                    }}
-                />
+                <input className="search-input" placeholder="Search items..." type="text"></input>
                 <IconButton sx={{ "&:hover": { backgroundColor: "RGBA(163,163,163,0.50)" } }}>
                     <SearchIcon sx={{ color: "#cccccc" }}></SearchIcon>
                 </IconButton>

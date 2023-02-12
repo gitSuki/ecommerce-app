30ea7e655747ae3aaff5c34069ce95989f075ef8
gitSuki <collinbrown226@gmail.com>
2023-02-12 15:54:35 -0600

    Add search bar icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,8 @@
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { setCartIsOpen } from "../state/cart";
+import { IconButton } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 import "./Navbar.css";
 
 const Navbar = (): JSX.Element => {
@@ -12,6 +14,12 @@ const Navbar = (): JSX.Element => {
     return (
         <nav className="navbar">
             <h1>Store Name</h1>
+            <form className="search-section">
+                <input type="text" placeholder="Search items..." />
+                <IconButton sx={{ '&:hover': {backgroundColor: "RGBA(163,163,163,0.50)"}}} >
+                    <SearchIcon className="search-icon"></SearchIcon>
+                </IconButton>
+            </form>
             <div>
                 <div>Browse</div>
                 <div>Search</div>
